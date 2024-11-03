c503dcae6d2b8bdf8d264f81565f09bb41ab70b4
Jorge Barrientos <elgigototo@hotmail.com>
2024-11-03 17:30:01 -0600

    add view scheme for dashboard

--- jetrix/src/components/protected_routes/dashboard/dashboard.js
+++ jetrix/src/components/protected_routes/dashboard/dashboard.js
@@ -1,30 +1,44 @@
 import React from "react";
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
+import Button from "@mui/material/Button";
 import { styled } from '@mui/material/styles';
+import './dashboard.css'
 
 
-const DemoPaper = styled(Paper)(({ theme }) => ({
-    width: 120,
-    height: 120,
-    padding: theme.spacing(2),
-    ...theme.typography.body2,
-    textAlign: 'center',
-}));
 
 const dashboard = () => {
 
     return (
-        <div>
-
-            <h1>asdfasdfadsf</h1>
-            <Stack direction={"row"} spacing={2}>
-                <DemoPaper square={false}><h1>demo demo demo</h1></DemoPaper>
+        <div className="mainDashboard">
+            <Stack  direction={"column"} spacing={5}>
+                <Stack direction={"row"} spacing={5}>
+                    <Button className="custom-button" ><h2>test</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                    <Button className="custom-button"><h2>test3</h2></Button>
+                    <Button className="custom-button"><h2>test4</h2></Button>
+                    <Button className="custom-button"><h2>test5</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                </Stack>
+                <Stack direction={"row"} spacing={5}>
+                    <Button className="custom-button" ><h2>test</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                    <Button className="custom-button"><h2>test3</h2></Button>
+                    <Button className="custom-button"><h2>test4</h2></Button>
+                    <Button className="custom-button"><h2>test5</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                </Stack>
+                <Stack direction={"row"} spacing={5}>
+                    <Button className="custom-button" ><h2>test</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                    <Button className="custom-button"><h2>test3</h2></Button>
+                    <Button className="custom-button"><h2>test4</h2></Button>
+                    <Button className="custom-button"><h2>test5</h2></Button>
+                    <Button className="custom-button"><h2>test2</h2></Button>
+                </Stack>
             </Stack>
         </div>
     )
-
-
 }
 
 export default dashboard;
